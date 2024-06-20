ce35170d1c0e504c2b63979ef5eaaf67d58cf202
RishiAstra <40644327+RishiAstra@users.noreply.github.com>
2024-06-20 20:23:22 +0000

    not working seq len thing

--- fms/utils/evaluation.py
+++ fms/utils/evaluation.py
@@ -38,16 +38,46 @@ class FMSEvalHarnessLM(LM):
         )
         if not len(context_ids):
             context_ids = [self.tokenizer.bos_token_id]
+        
+        max_len = self.wrapped_model.config.max_expected_seq_len
 
         continuation_ids = self.tokenizer.convert_tokens_to_ids(
             self.tokenizer.tokenize(continuation)
         )
         input_ids = context_ids + continuation_ids[:-1]
+        remaining = None
+        if len(input_ids) > max_len:
+            remaining = input_ids[max_len:]
+            input_ids = input_ids[:max_len]
         input_ids = torch.tensor(
             input_ids, dtype=torch.long, device=self.device
-        ).unsqueeze(0)
-        logits = F.log_softmax(self.wrapped_model(input_ids)[0], -1)
-        continuation_probs = logits[len(context_ids) - 1 :]
+        ).unsqueeze(0).cuda()
+
+        model_out, curr_cache = self.wrapped_model(input_ids, use_cache=True)
+        logits = F.log_softmax(model_out[0], -1).cpu()
+
+        if remaining is not None:
+            for i in range(0, len(remaining), 2048):
+                cache_len = curr_cache[0][0].shape[2]
+                curr_tokens = remaining[i : min(len(remaining), i + 2048)]
+                if cache_len >= max_len:
+                    curr_cache = [(layer_keys[:, :, -max_len+1:], layer_vals[:, :, -max_len+1:]) for layer_keys, layer_vals in curr_cache]
+                    cache_len = curr_cache[0][0].shape[2]
+
+                input_len = len(curr_tokens)
+                total_len = cache_len + input_len
+                assert total_len < 4096
+                i_range = torch.arange(cache_len, total_len).view(-1, 1)
+                j_range = torch.arange(0, total_len)
+                mask = torch.where((j_range <= i_range) & (i_range < j_range + max_len), 0, -torch.inf).to(input_ids.device)
+                mask = mask.broadcast_to(1, 32, input_len, total_len)
+
+                token_ids = torch.tensor([curr_tokens], dtype=torch.long, device=self.device)
+                model_out, curr_cache = self.wrapped_model(token_ids, use_cache=True, past_key_value_states=curr_cache, mask=mask)
+                curr_logits = F.log_softmax(model_out[0], -1).cpu()
+                logits = torch.cat([logits, curr_logits], dim=0)
+
+        continuation_probs = logits[:, len(context_ids) - 1:]
         loglikelihood = torch.gather(
             continuation_probs, 1, torch.tensor(continuation_ids).unsqueeze(1)
         ).squeeze()
@@ -65,6 +95,12 @@ class FMSEvalHarnessLM(LM):
     def loglikelihood_rolling(
         self, requests: List[Instance]
     ) -> List[Tuple[float, bool]]:
+        # TODO: check implementation
+        result = []
+        for request in requests:
+            continuation = request.args[0]
+            result.append(self.loglikelihood_one("<|endoftext|>", continuation)[0])
+        return result
         raise NotImplementedError("not implemented yet")
 
     def generate_until(self, requests: List[Instance]) -> List[str]:
