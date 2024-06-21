73696144cb14edbbf30d3769f3d5c5b21f4c4688
krishagarwal <krishagarwal@live.com>
2024-06-21 15:37:02 +0000

    sliding window with double max len potentially working

--- fms/utils/evaluation.py
+++ fms/utils/evaluation.py
@@ -8,6 +8,7 @@ from lm_eval.api.registry import register_model  # type: ignore
 from torch import nn
 
 from fms.utils import tokenizers
+from tqdm import tqdm
 
 
 @register_model("fms")
@@ -45,39 +46,32 @@ class FMSEvalHarnessLM(LM):
             self.tokenizer.tokenize(continuation)
         )
         input_ids = context_ids + continuation_ids[:-1]
-        remaining = None
-        if len(input_ids) > max_len:
-            remaining = input_ids[max_len:]
-            input_ids = input_ids[:max_len]
         input_ids = torch.tensor(
             input_ids, dtype=torch.long, device=self.device
         ).unsqueeze(0).cuda()
 
-        model_out, curr_cache = self.wrapped_model(input_ids, use_cache=True)
-        logits = F.log_softmax(model_out[0], -1).cpu()
-
-        if remaining is not None:
-            for i in range(0, len(remaining), 2048):
+        curr_cache = None
+        logits = None
+        for i in range(0, input_ids.shape[1], max_len):
+            cache_len = curr_cache[0][0].shape[2] if curr_cache is not None else 0
+            if cache_len >= max_len:
+                curr_cache = [(layer_keys[:, :, -max_len+1:], layer_vals[:, :, -max_len+1:]) for layer_keys, layer_vals in curr_cache]
                 cache_len = curr_cache[0][0].shape[2]
-                curr_tokens = remaining[i : min(len(remaining), i + 2048)]
-                if cache_len >= max_len:
-                    curr_cache = [(layer_keys[:, :, -max_len+1:], layer_vals[:, :, -max_len+1:]) for layer_keys, layer_vals in curr_cache]
-                    cache_len = curr_cache[0][0].shape[2]
+            
+            curr_tokens = input_ids[:, i : min(input_ids.shape[1], i + max_len)]
+            input_len = curr_tokens.shape[1]
 
-                input_len = len(curr_tokens)
-                total_len = cache_len + input_len
-                assert total_len < 4096
-                i_range = torch.arange(cache_len, total_len).view(-1, 1)
-                j_range = torch.arange(0, total_len)
-                mask = torch.where((j_range <= i_range) & (i_range < j_range + max_len), 0, -torch.inf).to(input_ids.device)
-                mask = mask.broadcast_to(1, 32, input_len, total_len)
+            total_len = cache_len + input_len
+            assert total_len < 2 * max_len
+            i_range = torch.arange(cache_len, total_len).view(-1, 1)
+            j_range = torch.arange(0, total_len).view(1, -1)
+            mask = ((j_range <= i_range) & (j_range > i_range - max_len)).to(input_ids.device).broadcast_to(1, 32, input_len, total_len)
 
-                token_ids = torch.tensor([curr_tokens], dtype=torch.long, device=self.device)
-                model_out, curr_cache = self.wrapped_model(token_ids, use_cache=True, past_key_value_states=curr_cache, mask=mask)
-                curr_logits = F.log_softmax(model_out[0], -1).cpu()
-                logits = torch.cat([logits, curr_logits], dim=0)
+            model_out, curr_cache = self.wrapped_model(curr_tokens, use_cache=True, past_key_value_states=curr_cache, mask=mask)
+            curr_logits = F.log_softmax(model_out[0], -1).cpu()
+            logits = curr_logits if logits is None else torch.cat([logits, curr_logits], dim=0)
 
-        continuation_probs = logits[:, len(context_ids) - 1:]
+        continuation_probs = logits[len(context_ids) - 1:, :]
         loglikelihood = torch.gather(
             continuation_probs, 1, torch.tensor(continuation_ids).unsqueeze(1)
         ).squeeze()
@@ -87,7 +81,7 @@ class FMSEvalHarnessLM(LM):
 
     def loglikelihood(self, requests: List[Instance]) -> List[Tuple[float, bool]]:
         result = []
-        for request in requests:
+        for request in tqdm(requests):
             context, continuation = request.args
             result.append(self.loglikelihood_one(context, continuation))
         return result
@@ -97,9 +91,9 @@ class FMSEvalHarnessLM(LM):
     ) -> List[Tuple[float, bool]]:
         # TODO: check implementation
         result = []
-        for request in requests:
+        for request in tqdm(requests):
             continuation = request.args[0]
-            result.append(self.loglikelihood_one("<|endoftext|>", continuation)[0])
+            result.append(self.loglikelihood_one("", continuation)[0])
         return result
         raise NotImplementedError("not implemented yet")
 
